ae39d46b0d67054810c1f07cdebbfbc8423f585e
Patrício Marin <patricioemarin@hotmail.com>
2024-04-25 09:19:31 -0300

    Dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -61,8 +62,30 @@ class EventController extends Controller
     }
 
     public function show($id) {
+        
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', '=', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+
+    }
+
+    public function dashboard() {
+        
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
+
+    }
+
+    public function destroy($id) {
+
+        $event = Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg','Evento excluído com sucesso!');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,10 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 });
