6ce7b18da1e9def45800a012b094e65ccbea0478
Patrício Marin <patricioemarin@hotmail.com>
2024-04-27 13:34:35 -0300

    Relação Participantes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::put('/events/update/{id}',[EventController::class, 'update'])->middleware
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
+Route::post('events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
 Route::get('/contact', function () {
     return view('contact');
 });
